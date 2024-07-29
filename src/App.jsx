7133ce6be367cfcf9b7631d3aba93557bc160dcc
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-07-28 21:14:49 -0700

    added a function onFocus to see book details

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Routes, Route, useNavigate} from 'react-router-dom'
 
 
 function App() {
+  const [bookTitles, setBookTitles] = useState([]);
+  const [bookId, setBookId] = useState('');
+  const [userNameInfo, setUserNameInfo] = useState('');
+
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books`)
+      const responseJson = await response.json();
+      setBookTitles(responseJson.books.map(book => book.title));
+      console.log(bookTitles);
+    }
+    apiCall();
+  },[])
+
+
  
 
   return (
     <>
      
+      <Routes>
+        <Route to="/" element='Home'>Home</Route>
+      </Routes>
+     <ul>
+      {bookTitles.map((title, index) => (
+        <li key={index}>{title}</li>
+      ))}
+     </ul>
     </>
-  )
+  );
 }
 
 export default App
